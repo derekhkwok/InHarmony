fc30118a4735c70c34d83f8a8c0cc42d9ef6e833
mtsangcl102 <mtsang911002@gmail.com>
2019-01-26 07:34:42 +0800

    Alan's Push
    Alan's Push

---
+++ Assets/Scripts/GamePlayUI/UI_Room.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_Room : MonoBehaviour, IDragHandler
+{
+    public int roomIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = Input.mousePosition;
+    }
+}
---
+++ Assets/Scripts/GamePlayUI/UI_RoomsPage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_RoomsPage : MonoBehaviour
+{
+    public GameObject[] defaultRooms;
+    List<GameObject> _rooms = new List<GameObject>();
+
+    private int[] _toWaitRoom;
+    private int[] _inShowRoom;
+
+    private void Start()
+    {
+        SetUpRooms(new int[] { 2, 3, 7 });
+    }
+
+    public void SetUpRooms( int[] thisStageRooms )
+    {
+        for ( int i = 0; i < thisStageRooms.Length; i++)
+        {
+            GameObject _tempRoom = Instantiate(defaultRooms[thisStageRooms[i]]) as GameObject;
+            _tempRoom.GetComponent<RectTransform>().SetParent( this.GetComponent< RectTransform >() );
+            _rooms.Add(_tempRoom);
+            _tempRoom.transform.localPosition = new Vector3(75f, _rooms.Count * 150f -50f, 0f);
+        }
+
+    }
+}
