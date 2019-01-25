86dd34242078c3686d8e238ad9140b10451c2b96
Derek Kwok <derekkwokhf@gmail.com>
2019-01-26 05:44:01 +0800

    resolve conflict

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -42,17 +42,10 @@ public class StageManager : MonoBehaviour
         currentStage = GameObject.Instantiate(stagePrefabs[stage]) as GameObject;
         currentStage.transform.parent = transform;
 
-<<<<<<< HEAD
-/*        foreach (Room r in GetComponentInChildren<Room>())
-        {
-
-        }*/
-=======
         foreach (Room r in GetComponentsInChildren<Room>())
         {
             currentRooms.Add(r.id, r);
         }
->>>>>>> 69581751b524cf0a8ced0432db73e38bc0c40072
 
         //foreach (Person r in GetComponentsInChildren<Room>())
         //{
@@ -66,17 +59,6 @@ public class StageManager : MonoBehaviour
         if (currentStage == null) return false;
 
         // Check room connect conditions
-
-
-
-<<<<<<< HEAD
         return true;
-=======
-<<<<<<< HEAD
-//        foreach
-=======
-        //foreach
->>>>>>> 69581751b524cf0a8ced0432db73e38bc0c40072
->>>>>>> 9b1cbf00d2b36247b3a60e2405bf288e01b6aa48
     }
 }
