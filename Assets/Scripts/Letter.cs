a3b8eda35f86499baf1b3b1808a9b9d4799e1591
Derek Kwok <derekkwokhf@gmail.com>
2019-01-26 01:46:58 +0800

    GameManager;Letter update

---
+++ Assets/Scripts/Letter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Letter : ScriptableObject
+{
+    public int expiryTime;
+    public string message;
+    public Sprite tex;
+}
