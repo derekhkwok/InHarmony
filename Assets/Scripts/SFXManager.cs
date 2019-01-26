2e7d09022ddb2e7b0e9bdab3d08f48a9fef6f319
ImFreeRider <keith1_leung@yahoo.com.hk>
2019-01-26 19:19:11 +0800

    SFX and Menu

--- Assets/Scripts/SFXManager.cs
+++ Assets/Scripts/SFXManager.cs
@@ -13,6 +13,11 @@ public class SFXManager : MonoBehaviour
         WELCOME,
         BTNPRESS,
         TRANSITION,
+        buildingCollapse,
+        buildingCollapse2,
+        powerUp,
+        powerUp2,
+        jump
     }
 
     void Awake()
