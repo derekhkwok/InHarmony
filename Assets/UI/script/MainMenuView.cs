b24eaa7cd248f760d18417ca296cb938821a0818
ImFreeRider <keith1_leung@yahoo.com.hk>
2019-01-26 12:37:42 +0800

    MainMenu

---
+++ Assets/UI/script/MainMenuView.cs
@@ -0,0 +1,219 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MainMenuView : MonoBehaviour
+{
+    public static bool isFirstEnter = true;
+
+    public int currentLv = 1;
+    public float tileStartY;
+    public float lowestTilePosY;
+    public float tilePadding;
+    public Animation startAnim;
+
+    public SpriteRenderer[] tileSprs;
+    public TextMesh[] tileText;
+    public GameObject groundPS;
+    public GameObject skyPS;
+
+    public SpriteRenderer rulerLow;
+    public SpriteRenderer rulerMid;
+    public Transform rulerMidPt;
+    public SpriteRenderer rulerHigh;
+
+    private bool canClick = false;
+    private Vector3 tileOriScale;
+
+    public GameObject parent;
+
+
+    public static MainMenuView instance;
+    public static MainMenuView SummonMenu()
+    {
+        if( instance == null )
+        {
+            instance = Instantiate(Resources.Load("MainMenu_VP"), new Vector3( 0f, 2f, 0f ), Quaternion.identity, null ) as MainMenuView;
+        }
+        return instance;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent.transform.localPosition = new Vector3(0f, 0f, -10f);
+        tileOriScale = tileSprs[0].transform.localScale;
+        for (int i = 0; i < currentLv - 1; i++)
+        {
+            tileSprs[i].transform.localPosition = new Vector3(tileSprs[i].transform.localPosition.x,
+                tileSprs[i].transform.localPosition.y, lowestTilePosY + tilePadding * i);
+        }
+        for (int i = currentLv - 1; i < tileSprs.Length; i++)
+        {
+            tileSprs[i].transform.localPosition = new Vector3(tileSprs[i].transform.localPosition.x,
+                tileSprs[i].transform.localPosition.y, tileStartY);
+            if(i >= currentLv - 1)
+            {
+                tileSprs[i].gameObject.SetActive(false);
+            }
+        }
+        if ( isFirstEnter )
+        {
+            isFirstEnter = false;
+            startAnim.Play();
+            Invoke("DropTile", 2f);
+            iTween.MoveTo(parent, iTween.Hash(
+                "z", 0f,
+                "time", 1f,
+                "islocal", true,
+                "easetype", iTween.EaseType.easeOutCubic
+                ));
+        }
+        else
+        {
+            iTween.MoveTo(parent, iTween.Hash(
+                "z", 0f,
+                "time", 1f,
+                "islocal", true,
+                "easetype", iTween.EaseType.easeOutCubic,
+                "oncomplete", "DropTile",
+                "oncompletetarget", gameObject
+                ));
+        }
+    }
+
+    public void DropTile()
+    {
+        tileSprs[currentLv - 1].gameObject.SetActive(true);
+        iTween.MoveTo(tileSprs[currentLv - 1].gameObject, iTween.Hash(
+            "z", lowestTilePosY + tilePadding * ( currentLv - 1),
+            "time", 0.2f * ( 7 - currentLv ),
+            "islocal", true,
+            "easetype", iTween.EaseType.easeInCubic,
+            "oncomplete", "Dust",
+            "oncompletetarget", gameObject
+            ));
+    }
+
+    public void Dust()
+    {
+        if (currentLv == 1)
+        {
+            groundPS.gameObject.SetActive(false);
+            groundPS.gameObject.SetActive(true);
+        }
+        else
+        {
+            skyPS.transform.localPosition = new Vector3(skyPS.transform.localPosition.x, skyPS.transform.localPosition.y, lowestTilePosY + tilePadding * (currentLv - 1) - tilePadding / 2f);
+            skyPS.gameObject.SetActive(false);
+            skyPS.gameObject.SetActive(true);
+        }
+        StartCoroutine(ShowFloorText());
+    }
+
+    public IEnumerator ShowFloorText()
+    {
+        /*iTween.ValueTo(this.gameObject, iTween.Hash(
+            "from", 0f,
+            "to", 1f,
+            "time", 0.5f,
+            "easetype", iTween.EaseType.linear
+            //"onupdate", "UpdateColor"
+            ));*/
+
+        yield return new WaitForSeconds(0.5f);
+
+        iTween.MoveTo(rulerLow.gameObject, iTween.Hash(
+            "z", -1.8f,
+            "time", 0.3f,
+            "islocal", true,
+            "easetype", iTween.EaseType.easeInOutQuad
+            ));
+
+        iTween.MoveTo(rulerHigh.gameObject, iTween.Hash(
+            "y", 5.7f / 6f * currentLv,
+            "time", 0.3f,
+            "islocal", true,
+            "easetype", iTween.EaseType.easeInOutQuad
+            ));
+        iTween.ScaleTo(rulerMidPt.gameObject, iTween.Hash(
+            "x", 40f / 6f * currentLv,
+            "time", 0.3f,
+            "islocal", true,
+            "easetype", iTween.EaseType.easeInOutQuad
+            ));
+        tileText[currentLv - 1].color = Color.red;
+        tileText[currentLv - 1].fontSize = 115;
+        for ( int i = 0; i < currentLv; i ++ )
+        {
+            iTween.MoveTo(tileText[i].gameObject, iTween.Hash(
+                "x" , -5.77f,
+                "time", 0.3f,
+                "islocal", true,
+                "delay", 0.1f * i,
+                "easetype", iTween.EaseType.easeOutCubic
+                ));
+        }
+        yield return new WaitForSeconds(currentLv * 0.1f + 0.3f);
+        canClick = true;
+    }
+
+    /*public void UpdateColor( float newVal )
+    {
+        Debug.Log(newVal);
+        rulerLow.color = new Color(rulerLow.color.r, rulerLow.color.g, rulerLow.color.b, (float)newVal);
+        rulerMid.color = new Color(rulerMid.color.r, rulerMid.color.g, rulerMid.color.b, (float)newVal);
+        rulerHigh.color = new Color(rulerHigh.color.r, rulerHigh.color.g, rulerHigh.color.b, (float)newVal);
+    }*/
+
+    // Update is called once per frame
+    float time = 0f;
+    void Update()
+    {
+        if (canClick)
+        {
+            time += Time.deltaTime * 3f;
+            float y = Mathf.Sin(time - 45f) * 0.05f + 1.05f;
+            tileSprs[currentLv - 1].transform.localScale = new Vector3(tileOriScale.x * y, tileOriScale.y * y, 1f);
+
+            Vector3 pos = Vector3.zero;
+            if (Input.GetMouseButtonUp(0))
+            {
+                pos = Input.mousePosition;
+            }
+            else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                pos = Input.GetTouch(0).position;
+            }
+            if (pos != Vector3.zero)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(pos);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider != null && hit.collider.CompareTag("UIMenu"))
+                    {
+                        int stageID = int.Parse(hit.collider.name.Substring(4, 1));
+                        StartCoroutine( EnterStage(stageID) );
+                    }
+                }
+            }
+        }
+    }
+
+    public IEnumerator EnterStage(int id)
+    {
+        iTween.MoveTo(parent, iTween.Hash(
+            "z", -10f,
+            "time", 0.5f,
+            "islocal", true,
+            "easetype", iTween.EaseType.easeInCubic
+            ));
+        yield return new WaitForSeconds(0.5f);
+        // Enterstage
+
+        Destroy(this.gameObject);
+    }
+
+}
