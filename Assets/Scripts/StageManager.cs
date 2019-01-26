d9eba0e7280dd56942e7460ad606f08c1dc57062
Derek Kwok <derekkwokhf@gmail.com>
2019-01-26 22:00:29 +0800

    stage update

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class StageManager : MonoBehaviour
+public partial class StageManager : MonoBehaviour
 {
     [Header("Stage Library")]
     public GameObject[] stagePrefabs;
